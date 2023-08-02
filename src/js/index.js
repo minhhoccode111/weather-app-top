5fac0a89846a8a2a1c661c799fddaa98bb0e9566
minhhoccode111 <minhhoccode111@gmail.com>
2023-08-02 18:11:30 +0700

    Setup files

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('Hello, World! From index.js');
+
+const factory = (() => {
+  return {};
+})();
+
+const display = (() => {
+  return {};
+})();
+
+const data = (() => {
+  return {};
+})();
+
+const app = (() => {
+  return {};
+})();
+
+const listener = (() => {
+  return {};
+})();
